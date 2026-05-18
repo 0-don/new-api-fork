d3be99613fa9de501523b1d1910eb0f26a9d50cb
0-don <don.cryptus@gmail.com>
2026-05-18 12:37:06 +0200

    fix: exclude models with suffix '[1m]' from pricing update

--- model/pricing.go
+++ model/pricing.go
@@ -293,6 +293,9 @@ func updatePricing() {
 
 	pricingMap = make([]Pricing, 0)
 	for model, groups := range modelGroupsMap {
+		if strings.HasSuffix(model, "[1m]") {
+			continue
+		}
 		pricing := Pricing{
 			ModelName:              model,
 			EnableGroup:            groups.Items(),
